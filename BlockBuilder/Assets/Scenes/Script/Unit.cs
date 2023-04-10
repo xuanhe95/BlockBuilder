e6728988d646629c6c1555f4be337a5c231f3045
ishmael816 <ishmael816@users.noreply.github.com>
2023-04-10 16:57:14 -0700

    Ish410

---
+++ BlockBuilder/Assets/Scenes/Script/Unit.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public MeshPrefab mesh;
+    public int level;
+    public List<string> bitmask;
+    private void Start()
+    {
+        
+    }
+
+    public void InitialUnit(List<List<Vector3>> listList)
+    {
+        foreach (List<Vector3> VARIABLE in listList)
+        {
+            //initial
+        }
+    }
+}
